17dfcf2a74bea2eb6e72050a735a535d98e50aeb
InvictusMalum <tylerjweed@gmail.com>
2022-02-03 14:20:04 -0500

    no change? safety

--- scripts/PeriodicTable.js
+++ scripts/PeriodicTable.js
@@ -1,9 +1,3 @@
-fetch("elements.json")
-.then(response => {
-   return response.json();
-})
-.then(jsondata => console.log(jsondata));
-console.log(jsonData);
 class PeriodicTable {
     constructor() {
         this.elements = [];
